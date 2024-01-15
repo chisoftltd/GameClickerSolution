dd7c3a4cc728c1135e3530c6826315765aedd2d2
chisoftltd <chisoftltd@gmail.com>
2024-01-15 00:20:41 +0000

    Create dictionary of store items and prices of Clicker Solution

--- main.py
+++ main.py
@@ -42,37 +42,37 @@ while True:
                 item_prices.append(cost)
                 print(item_prices)
 
-                # Create dictionary of store items and prices
-                cookie_upgrades = {}
-                for n in range(len(item_prices)):
-                    cookie_upgrades[item_prices[n]] = item_ids[n]
+        # Create dictionary of store items and prices
+        cookie_upgrades = {}
+        for n in range(len(item_prices)):
+            cookie_upgrades[item_prices[n]] = item_ids[n]
 
-                # Get current cookie count
-                money_element = driver.find_element(by=By.ID, value="money").text
-                if "," in money_element:
-                    money_element = money_element.replace(",", "")
-                cookie_count = int(money_element)
+        # Get current cookie count
+        money_element = driver.find_element(by=By.ID, value="money").text
+        if "," in money_element:
+            money_element = money_element.replace(",", "")
+        cookie_count = int(money_element)
 
-                # Find upgrades that we can currently afford
-                affordable_upgrades = {}
-                for cost, id in cookie_upgrades.items():
-                    if cookie_count > cost:
-                        affordable_upgrades[cost] = id
-                        print(affordable_upgrades)
+        # Find upgrades that we can currently afford
+        affordable_upgrades = {}
+        for cost, id in cookie_upgrades.items():
+            if cookie_count > cost:
+                affordable_upgrades[cost] = id
+                print(affordable_upgrades)
 
-                # Purchase the most expensive affordable upgrade
-                highest_price_affordable_upgrade = max(affordable_upgrades)
-                print(highest_price_affordable_upgrade)
-                to_purchase_id = affordable_upgrades[highest_price_affordable_upgrade]
+        # Purchase the most expensive affordable upgrade
+        highest_price_affordable_upgrade = max(affordable_upgrades)
+        print(highest_price_affordable_upgrade)
+        to_purchase_id = affordable_upgrades[highest_price_affordable_upgrade]
 
-                driver.find_element(by=By.ID, value=to_purchase_id).click()
+        driver.find_element(by=By.ID, value=to_purchase_id).click()
 
-                # Add another 5 seconds until the next check
-                timeout = time.time() + 5
+        # Add another 5 seconds until the next check
+        timeout = time.time() + 5
 
-            # After 5 minutes stop the bot and check the cookies per second count.
-            if time.time() > five_min:
-                cookie_per_s = driver.find_element(by=By.ID, value="cps").text
-                print(cookie_per_s)
-                break
+    # After 5 minutes stop the bot and check the cookies per second count.
+    if time.time() > five_min:
+        cookie_per_s = driver.find_element(by=By.ID, value="cps").text
+        print(cookie_per_s)
+        break
 
